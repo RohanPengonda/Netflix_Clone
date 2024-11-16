8a2b7240fa44262a271ceb9c32a5fc475cfa84bc
RohanPengonda <rpengonda1@gmail.com>
2024-11-16 12:54:28 +0530

    Home Updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,23 @@
 // import React from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            qui impedit modi excepturi minus laborum asperiores, blanditiis est
+            recusandae hic!
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
