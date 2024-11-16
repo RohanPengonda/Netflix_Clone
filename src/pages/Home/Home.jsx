54e20e4e953f09497ccead30758d7ba2953b7f42
RohanPengonda <rpengonda1@gmail.com>
2024-11-16 13:58:23 +0530

    HomePage Updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 import hero_banner from "../../assets/hero_banner.jpg";
 import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
 const Home = () => {
   return (
     <div className="home">
@@ -16,6 +18,16 @@ const Home = () => {
             qui impedit modi excepturi minus laborum asperiores, blanditiis est
             recusandae hic!
           </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={play_icon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={info_icon} alt="" />
+              More info
+            </button>
+          </div>
         </div>
       </div>
     </div>
