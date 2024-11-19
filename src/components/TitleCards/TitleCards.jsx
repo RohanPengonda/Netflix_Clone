a0eb1d240285e0110b5a4cfe17cef91b4bf34f08
RohanPengonda <rpengonda1@gmail.com>
2024-11-19 19:22:47 +0530

    API Data Added

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,27 +1,52 @@
 // import React from "react";
 import "./TitleCards.css";
-import cards_data from "../../assets/cards/Cards_data";
-// import { useEffect, useRef } from "react";
+// import cards_data from "../../assets/cards/Cards_data";
+import { useEffect, useState } from "react";
 
 const TitleCards = ({ title, category }) => {
+  const [apiData, setapiData] = useState([]);
   // For Mouse Wheel Scroll
   // const cardsRef = useRef();
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3N2QzODRiMjk4ZGU3YjNjMTJkNDk4MzY5MDFhZDIxNiIsIm5iZiI6MTczMjAyMDM3Ny42NDI5Mjg2LCJzdWIiOiI2NzNjODZjN2U3NzJjY2MxNTY0NTJmMDIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.fNUES8jObM4fmoEV_kcxN9fgE4_UrKGSd0TKMCsm7kk",
+    },
+  };
+
   // const handleWheel = (event) => {
   //   event.preventDefault();
   //   cardsRef.current.srollLeft += event.deltaY;
   // };
-  // useEffect(() => {
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category ? category : "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setapiData(res.results))
+      .catch((err) => console.error(err));
+  });
+
+  //     useEffect(() => {
   //   cardsRef.current.addEventListener("wheel", handleWheel);
   // }, []);
   return (
     <div className="titlecards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" /*ref={cardsRef}*/>
-        {cards_data.map((card, index) => {
+        {apiData.map((card, index) => {
           return (
             <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
+              <img
+                src={"https://image.tmdb.org/t/p/w500" + card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
             </div>
           );
         })}
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,8 +1,50 @@
 // import React from "react";
 import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useEffect, useState } from "react";
 
 const Player = () => {
-  return <div className="player"></div>;
+  const [apiData, setapiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: "",
+  });
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3N2QzODRiMjk4ZGU3YjNjMTJkNDk4MzY5MDFhZDIxNiIsIm5iZiI6MTczMjAyMDM3Ny42NDI5Mjg2LCJzdWIiOiI2NzNjODZjN2U3NzJjY2MxNTY0NTJmMDIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.fNUES8jObM4fmoEV_kcxN9fgE4_UrKGSd0TKMCsm7kk",
+    },
+  };
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/912649/videos?language=en-US",
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setapiData(res.results[0]))
+      .catch((err) => console.error(err));
+  });
+  return (
+    <div className="player">
+      <img src={back_arrow_icon} alt="" />
+      <iframe
+        width="90%"
+        height="90%"
+        src="https://www.youtube.com/embed/lpx2zFkapIk?si=0M8-LDMpx52bLpdb"
+        title="trailer"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <div className="player-info">
+        <p>Published Date</p>
+        <p>Name</p>
+        <p>Type</p>
+      </div>
+    </div>
+  );
 };
 
 export default Player;
