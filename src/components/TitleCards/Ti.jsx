9d8edf8e1ce03916fd429a82aeb70d4029a91dda
RohanPengonda <rpengonda1@gmail.com>
2024-12-12 21:23:28 +0530

    Updated

--- src/components/TitleCards/Ti.jsx
+++ src/components/TitleCards/Ti.jsx
@@ -6,19 +6,8 @@ import { Link } from "react-router-dom";
 
 const TitleCards = ({ title, category }) => {
   const [apiData, setapiData] = useState([]);
-  const [release, setrelease] = useState();
   // For Mouse Wheel Scroll
   // const cardsRef = useRef();
-  const mouse_Enter = (rel) => {
-    return (
-      <>
-        console.log({rel});
-        <p>{rel}</p>
-        <p>{release}</p>
-        {/* <p>{card.vote_average}</p> */}
-      </>
-    );
-  };
   const options = {
     method: "GET",
     headers: {
@@ -50,34 +39,15 @@ const TitleCards = ({ title, category }) => {
   return (
     <div className="titlecards">
       <h2>{title ? title : "Popular on Netflix"}</h2>
-      <div
-        className="card-list"
-        /*ref={cardsRef}*/
-      >
+      <div className="card-list" /*ref={cardsRef}*/>
         {apiData.map((card, index) => {
           return (
-            <Link
-              to={`/player/${card.id}`}
-              // className="card"
-              key={index}
-              // onMouseEnter={() => {
-              //   mouse_Enter(card.release_date);
-              //   setrelease(card.release_date);
-              //   // console.log(card.original_title);
-              // }}
-            >
-              <div className="card">
-                <img
-                  src={"https://image.tmdb.org/t/p/w500" + card.backdrop_path}
-                  alt=""
-                />
-                <p className="title">{card.original_title}</p>
-                <div className="show">
-                  <p className="show1">{card.overview}</p>
-                  <p className="show1">{card.release_date}</p>
-                  <p className="show1">{card.vote_average}</p>
-                </div>
-              </div>
+            <Link to={`/player/${card.id}`} className="card" key={index}>
+              <img
+                src={"https://image.tmdb.org/t/p/w500" + card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
             </Link>
           );
         })}
